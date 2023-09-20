5d8a497d3bb7884d816e59645050cd4a755677b8
Surya <suryakant.subudhi@outlook.com>
2023-09-20 16:15:08 +0530

    📦 Added shadcn dialog

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <h1 className="text-black">
-        Dashboard
-    </h1>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
